2ffbdcd6f7f6a87e264e919c5b176885e4716cb9
Patrick Cho <pck.cho@gmail.com>
2020-06-03 00:05:24 +1200

    New Build

---
+++ client/build/precache-manifest.10e2df8e245799ac91d798d3dfb72ff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4890dc36e42bc11acc6cacbcbf80bf47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c78a172215ebd705b995",
+    "url": "/static/css/main.6b1e6d1b.chunk.css"
+  },
+  {
+    "revision": "01e6be3ddd73dd74c9b1",
+    "url": "/static/js/2.380c5a85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.380c5a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78a172215ebd705b995",
+    "url": "/static/js/main.a208102a.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
